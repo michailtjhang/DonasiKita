24dd4a40042efe1a6a09601bf4e15e527306543b
Christian Putri <christianptrii@gmail.com>
2024-11-18 21:52:56 +0800

    fixing styling

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,7 @@ use App\Http\Controllers\Backend\ConfigController;
 use App\Http\Controllers\Backend\DashboardController;
 
 Route::get('/', function () {
-<<<<<<< HEAD
     return view('front.home.home');
-=======
-    return view('welcome');
->>>>>>> aa2915288201a3f410ab797e4264ee177c5d6d51
 });
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
@@ -38,19 +34,5 @@ Route::group(['middleware' => ['auth', 'useradmin']], function () {
         Route::resource('category', CategoryController::class)
             ->only(['index', 'store', 'update', 'destroy']);
         Route::resource('article', BlogController::class);
-<<<<<<< HEAD
     });
 });
-
-
-//contoh route ke page lainnya
-// Route('/about', function () {
-//     return view('front.home.');
-// });
-=======
-
-        Route::post('/article/upload-image', [BlogController::class, 'uploadImage'])->name('article.uploadImage');
-
-    });
-});
->>>>>>> aa2915288201a3f410ab797e4264ee177c5d6d51
