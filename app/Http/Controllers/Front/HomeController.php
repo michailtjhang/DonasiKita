cf63f6287544b10bf4b7a0c190f69c9442387f0c
MICHAIL <72181056+michailtjhang@users.noreply.github.com>
2024-12-15 20:12:24 +0700

    refactor: add alert full partisipan event & change detail on landing pages

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use App\Models\Blog;
 use App\Models\Need;
 use App\Models\Event;
+use App\Models\Donation;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
@@ -12,9 +13,22 @@ class HomeController extends Controller
     public function home()
     {
         $last_articles = Blog::whereStatus(1)->take(3)->latest()->get();
-            $last_events = Event::whereStatus('upcoming')->take(3)->latest()->get();
-            $last_donation = Need::take(3)->latest()->get();
-            
+        $last_events = Event::whereStatus('upcoming')->take(3)->latest()->get();
+        $last_donation = Need::with(['donation', 'thumbnail'])
+            ->where('days_left', '>', now())
+            ->take(3)->latest()->get();
+
+        // Hitung total donasi yang disetujui untuk setiap "Need"
+        $last_donation->transform(function ($donation) {
+            $donation->total_donated = Donation::where('need_id', $donation->need_id)
+                ->where('status', 'approved')
+                ->sum('amount');
+            $donation->donator_count = Donation::where('need_id', $donation->need_id)
+                ->where('status', 'approved')
+                ->count(); // Hitung jumlah donatur (status disetujui)
+            return $donation;
+        });
+
         return view('front.home.home', [
             'last_articles' => $last_articles,
             'last_events' => $last_events,
